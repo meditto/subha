0c64d88495ca0c266643802c4a56c3752e1b9d0d
med <wvllvw@outlook.com>
2018-10-28 15:09:53 +0100

    fix serviceworker cache

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,16 +1,464 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
 
-workbox.routing.registerRoute(
-    new RegExp('.*\.js'),
-    workbox.strategies.networkFirst()
-);
-
-workbox.routing.registerRoute(
-    new RegExp('.*\.png'),
-    workbox.strategies.networkFirst()
-);
-
-workbox.routing.registerRoute(
-    new RegExp('.*\.css'),
-    workbox.strategies.networkFirst()
-);
+workbox.precaching.precacheAndRoute([
+  {
+    "url": "css/app.6ff932c1.css",
+    "revision": "2f9028a0babf4009fdfd23c760413bc4"
+  },
+  {
+    "url": "css/chunk-vendors.3c3429cd.css",
+    "revision": "ec35e94c55aedbceb2cfc0ff1fefc009"
+  },
+  {
+    "url": "favicon.ico",
+    "revision": "fe4792d482196a50cf9ae0d9d90b6493"
+  },
+  {
+    "url": "icons.json",
+    "revision": "172142be2f4df9510ea5bcefb86e72a5"
+  },
+  {
+    "url": "icons/android/android-launchericon-144-144.png",
+    "revision": "555d7525ed6df9f346e53bc3343c7262"
+  },
+  {
+    "url": "icons/android/android-launchericon-192-192.png",
+    "revision": "2a456aaa212a5089e615f09fb9085a07"
+  },
+  {
+    "url": "icons/android/android-launchericon-48-48.png",
+    "revision": "ba3ee81474cfb02859c1915f309f5022"
+  },
+  {
+    "url": "icons/android/android-launchericon-512-512.png",
+    "revision": "4f99b4999a18a992d1d2d2a2497b6ab2"
+  },
+  {
+    "url": "icons/android/android-launchericon-72-72.png",
+    "revision": "76350abf94c2b034d779bc7969905db7"
+  },
+  {
+    "url": "icons/android/android-launchericon-96-96.png",
+    "revision": "ea126f11101fe76b9ef9c97c8d49b834"
+  },
+  {
+    "url": "icons/chrome/chrome-extensionmanagementpage-48-48.png",
+    "revision": "ba3ee81474cfb02859c1915f309f5022"
+  },
+  {
+    "url": "icons/chrome/chrome-favicon-16-16.png",
+    "revision": "fbdddcc72c152a7b8962e17437ceb082"
+  },
+  {
+    "url": "icons/chrome/chrome-installprocess-128-128.png",
+    "revision": "89b0063186d31a1d033ad1ed45fdd375"
+  },
+  {
+    "url": "icons/firefox/firefox-general-128-128.png",
+    "revision": "89b0063186d31a1d033ad1ed45fdd375"
+  },
+  {
+    "url": "icons/firefox/firefox-general-16-16.png",
+    "revision": "fbdddcc72c152a7b8962e17437ceb082"
+  },
+  {
+    "url": "icons/firefox/firefox-general-256-256.png",
+    "revision": "324273124aeac54a70c7c00a7721b617"
+  },
+  {
+    "url": "icons/firefox/firefox-general-32-32.png",
+    "revision": "6ee90da380bd6a35de4f4623d9188a19"
+  },
+  {
+    "url": "icons/firefox/firefox-general-48-48.png",
+    "revision": "ba3ee81474cfb02859c1915f309f5022"
+  },
+  {
+    "url": "icons/firefox/firefox-general-64-64.png",
+    "revision": "507944561c940d1eccb014123b4073ea"
+  },
+  {
+    "url": "icons/firefox/firefox-general-90-90.png",
+    "revision": "4d823309dc307ef19437724b7b7311ab"
+  },
+  {
+    "url": "icons/firefox/firefox-marketplace-128-128.png",
+    "revision": "89b0063186d31a1d033ad1ed45fdd375"
+  },
+  {
+    "url": "icons/firefox/firefox-marketplace-512-512.png",
+    "revision": "4f99b4999a18a992d1d2d2a2497b6ab2"
+  },
+  {
+    "url": "icons/ios/ios-appicon-1024-1024.png",
+    "revision": "eb94b0d1eca5d0ad1d71a258f559d092"
+  },
+  {
+    "url": "icons/ios/ios-appicon-120-120.png",
+    "revision": "79220783d447884ed4d462fa495b77e1"
+  },
+  {
+    "url": "icons/ios/ios-appicon-152-152.png",
+    "revision": "1e69dd7850416b81db72b9ea3c8ced8e"
+  },
+  {
+    "url": "icons/ios/ios-appicon-180-180.png",
+    "revision": "bd91f566ea569e6b43ae70636473abf7"
+  },
+  {
+    "url": "icons/ios/ios-appicon-76-76.png",
+    "revision": "9a51a4dc129d4103c7038c491bcc4c00"
+  },
+  {
+    "url": "icons/ios/ios-launchimage-1024-768.png",
+    "revision": "694521245f14c36d03e688e88ed825c9"
+  },
+  {
+    "url": "icons/ios/ios-launchimage-1242-2208.png",
+    "revision": "10aac39a9ec34f889310f84cdba962c4"
+  },
+  {
+    "url": "icons/ios/ios-launchimage-1334-750.png",
+    "revision": "b50b9e8fa6669fd4df7fe90a079f1329"
+  },
+  {
+    "url": "icons/ios/ios-launchimage-1536-2048.png",
+    "revision": "f44a3f83b1880732b94caa2fc672d6af"
+  },
+  {
+    "url": "icons/ios/ios-launchimage-2048-1536.png",
+    "revision": "6f22efbe7d4c6cc2cb4c4dcd74fe6d67"
+  },
+  {
+    "url": "icons/ios/ios-launchimage-2208-1242.png",
+    "revision": "9bcb489fe9e3e8dafe36681fc019de22"
+  },
+  {
+    "url": "icons/ios/ios-launchimage-640-1136.png",
+    "revision": "1509e38b9751dbb176165df182ef7031"
+  },
+  {
+    "url": "icons/ios/ios-launchimage-640-960.png",
+    "revision": "31c4940499587c76f28d65647ff46595"
+  },
+  {
+    "url": "icons/ios/ios-launchimage-750-1334.png",
+    "revision": "6dec5f7a114ce06c03aefdc9a243b512"
+  },
+  {
+    "url": "icons/ios/ios-launchimage-768-1024.png",
+    "revision": "a2ea9cdd27f290532c3eff2411959352"
+  },
+  {
+    "url": "icons/windows/windows-smallsquare-24-24.png",
+    "revision": "5920ee4d28eebbd5578e78ed7f787728"
+  },
+  {
+    "url": "icons/windows/windows-smallsquare-30-30.png",
+    "revision": "13474ac1e87f639e9d1be804e8f075c4"
+  },
+  {
+    "url": "icons/windows/windows-smallsquare-42-42.png",
+    "revision": "a32409dca8acc02ddd6a5746779630a5"
+  },
+  {
+    "url": "icons/windows/windows-smallsquare-54-54.png",
+    "revision": "d62e87d79aa59e2909881d6a560bae18"
+  },
+  {
+    "url": "icons/windows/windows-splashscreen-1116-540.png",
+    "revision": "f0e6a377cc8c9eb9d821bf94faef36a2"
+  },
+  {
+    "url": "icons/windows/windows-splashscreen-620-300.png",
+    "revision": "3c77bea7877bcd4592b34d0871b32fb1"
+  },
+  {
+    "url": "icons/windows/windows-splashscreen-868-420.png",
+    "revision": "790d6e9d4772810699be7e291698e504"
+  },
+  {
+    "url": "icons/windows/windows-squarelogo-120-120.png",
+    "revision": "79220783d447884ed4d462fa495b77e1"
+  },
+  {
+    "url": "icons/windows/windows-squarelogo-150-150.png",
+    "revision": "5325d647249efd22442b93970e5a55d5"
+  },
+  {
+    "url": "icons/windows/windows-squarelogo-210-210.png",
+    "revision": "04dd6674e458c66e6d1ff4fef3676747"
+  },
+  {
+    "url": "icons/windows/windows-squarelogo-270-270.png",
+    "revision": "3d670d30a4b1fed6002f10a92bc8f917"
+  },
+  {
+    "url": "icons/windows/windows-storelogo-50-50.png",
+    "revision": "38fb2d1a98a7b3984546979a0498a307"
+  },
+  {
+    "url": "icons/windows/windows-storelogo-70-70.png",
+    "revision": "d8a5394aefee556cdeb95cb663f72bae"
+  },
+  {
+    "url": "icons/windows/windows-storelogo-90-90.png",
+    "revision": "4d823309dc307ef19437724b7b7311ab"
+  },
+  {
+    "url": "icons/windows/windowsphone-appicon-106-106.png",
+    "revision": "8aa1653c7f992c77983f35837fe6d236"
+  },
+  {
+    "url": "icons/windows/windowsphone-appicon-44-44.png",
+    "revision": "3ea39b166efea293fcac661ec08fe7d9"
+  },
+  {
+    "url": "icons/windows/windowsphone-appicon-62-62.png",
+    "revision": "a1bc8001e9e881568368702944c2ff57"
+  },
+  {
+    "url": "icons/windows/windowsphone-mediumtile-150-150.png",
+    "revision": "5325d647249efd22442b93970e5a55d5"
+  },
+  {
+    "url": "icons/windows/windowsphone-mediumtile-210-210.png",
+    "revision": "04dd6674e458c66e6d1ff4fef3676747"
+  },
+  {
+    "url": "icons/windows/windowsphone-mediumtile-360-360.png",
+    "revision": "42663c84602e3c700f34fe6ee1e99d42"
+  },
+  {
+    "url": "icons/windows/windowsphone-smalltile-170-170.png",
+    "revision": "6eaf2ee64ec9e798112d50ad141b1496"
+  },
+  {
+    "url": "icons/windows/windowsphone-smalltile-71-71.png",
+    "revision": "86b75c00db645e4c238431ccd29cfbb4"
+  },
+  {
+    "url": "icons/windows/windowsphone-smalltile-99-99.png",
+    "revision": "2bf280ca316a0ba686c830635a10565c"
+  },
+  {
+    "url": "icons/windows/windowsphone-storelogo-120-120.png",
+    "revision": "79220783d447884ed4d462fa495b77e1"
+  },
+  {
+    "url": "icons/windows/windowsphone-storelogo-50-50.png",
+    "revision": "38fb2d1a98a7b3984546979a0498a307"
+  },
+  {
+    "url": "icons/windows/windowsphone-storelogo-70-70.png",
+    "revision": "d8a5394aefee556cdeb95cb663f72bae"
+  },
+  {
+    "url": "icons/windows10/SplashScreen.scale-100.png",
+    "revision": "3c77bea7877bcd4592b34d0871b32fb1"
+  },
+  {
+    "url": "icons/windows10/SplashScreen.scale-125.png",
+    "revision": "206f6709d95819b5070c67bc37a110c7"
+  },
+  {
+    "url": "icons/windows10/SplashScreen.scale-150.png",
+    "revision": "f00959f7ade648b459851896d0544a8b"
+  },
+  {
+    "url": "icons/windows10/SplashScreen.scale-200.png",
+    "revision": "a2b1cf58eb58bfcff88cf5364343fe4e"
+  },
+  {
+    "url": "icons/windows10/SplashScreen.scale-400.png",
+    "revision": "86cf14b91bfff4a0e320b4549e64b3b3"
+  },
+  {
+    "url": "icons/windows10/Square150x150Logo.scale-100.png",
+    "revision": "5325d647249efd22442b93970e5a55d5"
+  },
+  {
+    "url": "icons/windows10/Square150x150Logo.scale-125.png",
+    "revision": "49a91ace94eb661659fb262b3c79460e"
+  },
+  {
+    "url": "icons/windows10/Square150x150Logo.scale-150.png",
+    "revision": "2397dafc578bedf7c1302b159326696b"
+  },
+  {
+    "url": "icons/windows10/Square150x150Logo.scale-200.png",
+    "revision": "94655abc28db6b683fbbaf564e5b6841"
+  },
+  {
+    "url": "icons/windows10/Square150x150Logo.scale-400.png",
+    "revision": "a0e7ebfc3e843283967fecfa80997ac8"
+  },
+  {
+    "url": "icons/windows10/Square310x310Logo.scale-100.png",
+    "revision": "4e581dc2c4677c338cf7020a240f99e1"
+  },
+  {
+    "url": "icons/windows10/Square310x310Logo.scale-125.png",
+    "revision": "d1efd89ec65024cc93b7d096c9911daa"
+  },
+  {
+    "url": "icons/windows10/Square310x310Logo.scale-150.png",
+    "revision": "bcbc670fc4a9e861c5d1297860be2df3"
+  },
+  {
+    "url": "icons/windows10/Square310x310Logo.scale-200.png",
+    "revision": "7878fa61ff32a2019781c3a00956feb1"
+  },
+  {
+    "url": "icons/windows10/Square310x310Logo.scale-400.png",
+    "revision": "1e24e5e7254b4a55eb52783a2859d643"
+  },
+  {
+    "url": "icons/windows10/Square44x44Logo.scale-100.png",
+    "revision": "3ea39b166efea293fcac661ec08fe7d9"
+  },
+  {
+    "url": "icons/windows10/Square44x44Logo.scale-125.png",
+    "revision": "b15728b193e2f636d6c039071ab87e9b"
+  },
+  {
+    "url": "icons/windows10/Square44x44Logo.scale-150.png",
+    "revision": "f4eeaed1ea7adbbbfc89410781e689e0"
+  },
+  {
+    "url": "icons/windows10/Square44x44Logo.scale-200.png",
+    "revision": "3edd9d7bf6cb6072cbe188fcd9077dce"
+  },
+  {
+    "url": "icons/windows10/Square44x44Logo.scale-400.png",
+    "revision": "91536b7ee2e44f4db2863e0f9205ad02"
+  },
+  {
+    "url": "icons/windows10/Square44x44Logo.targetsize-16_altform-unplated.png",
+    "revision": "fbdddcc72c152a7b8962e17437ceb082"
+  },
+  {
+    "url": "icons/windows10/Square44x44Logo.targetsize-16.png",
+    "revision": "fbdddcc72c152a7b8962e17437ceb082"
+  },
+  {
+    "url": "icons/windows10/Square44x44Logo.targetsize-24_altform-unplated.png",
+    "revision": "5920ee4d28eebbd5578e78ed7f787728"
+  },
+  {
+    "url": "icons/windows10/Square44x44Logo.targetsize-24.png",
+    "revision": "5920ee4d28eebbd5578e78ed7f787728"
+  },
+  {
+    "url": "icons/windows10/Square44x44Logo.targetsize-256_altform-unplated.png",
+    "revision": "324273124aeac54a70c7c00a7721b617"
+  },
+  {
+    "url": "icons/windows10/Square44x44Logo.targetsize-256.png",
+    "revision": "324273124aeac54a70c7c00a7721b617"
+  },
+  {
+    "url": "icons/windows10/Square44x44Logo.targetsize-48_altform-unplated.png",
+    "revision": "ba3ee81474cfb02859c1915f309f5022"
+  },
+  {
+    "url": "icons/windows10/Square44x44Logo.targetsize-48.png",
+    "revision": "ba3ee81474cfb02859c1915f309f5022"
+  },
+  {
+    "url": "icons/windows10/Square71x71Logo.scale-100.png",
+    "revision": "86b75c00db645e4c238431ccd29cfbb4"
+  },
+  {
+    "url": "icons/windows10/Square71x71Logo.scale-125.png",
+    "revision": "62d65bf7f99f8c64c26647cb7123ff53"
+  },
+  {
+    "url": "icons/windows10/Square71x71Logo.scale-150.png",
+    "revision": "4559b1477766122f3776ffb890c4b6ea"
+  },
+  {
+    "url": "icons/windows10/Square71x71Logo.scale-200.png",
+    "revision": "7dae5ac5aa7e157bfbe62636d8f41ff0"
+  },
+  {
+    "url": "icons/windows10/Square71x71Logo.scale-400.png",
+    "revision": "bc797a11c03f57703f23f4c78e4d7d39"
+  },
+  {
+    "url": "icons/windows10/StoreLogo.png",
+    "revision": "38fb2d1a98a7b3984546979a0498a307"
+  },
+  {
+    "url": "icons/windows10/StoreLogo.scale-100.png",
+    "revision": "38fb2d1a98a7b3984546979a0498a307"
+  },
+  {
+    "url": "icons/windows10/StoreLogo.scale-125.png",
+    "revision": "bddb162daeadf89646c6df040a289e7c"
+  },
+  {
+    "url": "icons/windows10/StoreLogo.scale-150.png",
+    "revision": "942941b77f9334a947b18ea5d6e06454"
+  },
+  {
+    "url": "icons/windows10/StoreLogo.scale-200.png",
+    "revision": "ba92063c52a1f292f83d41d572605f7a"
+  },
+  {
+    "url": "icons/windows10/StoreLogo.scale-400.png",
+    "revision": "4f69f9d2fc6c80a39626a98f706b189d"
+  },
+  {
+    "url": "icons/windows10/Wide310x150Logo.scale-100.png",
+    "revision": "7074138fe6cdbb2d0ea1469240b7eb60"
+  },
+  {
+    "url": "icons/windows10/Wide310x150Logo.scale-125.png",
+    "revision": "4754f319deba72615ac03a16584eb3cf"
+  },
+  {
+    "url": "icons/windows10/Wide310x150Logo.scale-150.png",
+    "revision": "46a781e52c6e00fe59547b291cc2c529"
+  },
+  {
+    "url": "icons/windows10/Wide310x150Logo.scale-200.png",
+    "revision": "3c77bea7877bcd4592b34d0871b32fb1"
+  },
+  {
+    "url": "icons/windows10/Wide310x150Logo.scale-400.png",
+    "revision": "a2b1cf58eb58bfcff88cf5364343fe4e"
+  },
+  {
+    "url": "img/background.48c32cd0.png",
+    "revision": "48c32cd052a266381948baa98986227b"
+  },
+  {
+    "url": "img/forground.e413c2da.png",
+    "revision": "e413c2da260dbde1bb3f79f4581eb019"
+  },
+  {
+    "url": "index.html",
+    "revision": "932e293a305fb13252c6704c42527e8e"
+  },
+  {
+    "url": "index.php",
+    "revision": "d18ae736807d20a4a8ade44366b38be5"
+  },
+  {
+    "url": "js/app.270be966.js",
+    "revision": "86592021a8642217f54eb25cfedf2b55"
+  },
+  {
+    "url": "js/app.c388543b.js",
+    "revision": "2b59836c55d96fb057554172ae0733f3"
+  },
+  {
+    "url": "js/chunk-vendors.a8bcade5.js",
+    "revision": "fcf2adc3a07abafe0d03a102bdc69af9"
+  },
+  {
+    "url": "manifest.json",
+    "revision": "8fd006a5692daf1fb1c86efc91b9bc2b"
+  }
+]);
